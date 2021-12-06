59c1566d763cdb3d78135a05316c8587e63999b6
Simo Nurmi <simo.aleksi.nurmi@gmail.com>
2021-12-07 00:17:58 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import moment from "moment";
+import { addDays } from "date-fns";
+import { Temporal, Intl as TemporalIntl } from "@js-temporal/polyfill";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>Moment: {moment().add(1, "days").format("D.MM.YYYY")}</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          date-fns:{" "}
+          {new Intl.DateTimeFormat("fi-FI").format(addDays(new Date(), 1))}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Temporal API polyfill:{" "}
+          {new TemporalIntl.DateTimeFormat("fi-FI", {
+            dateStyle: "short",
+          }).format(
+            Temporal.Now.zonedDateTime("gregory").add(
+              Temporal.Duration.from({ days: 1 })
+            )
+          )}
+        </p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
